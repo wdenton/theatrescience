2aa355ea90ca6c177aee82d7f59d5b232dfd1780
William Denton <wtd@pobox.com>
2020-01-08 14:19:04 -0500

    Rename files and roles

--- score/build.sh
+++ score/build.sh
@@ -1,13 +1,15 @@
 
 # https://en.wikibooks.org/wiki/LaTeX/Macros
 
-PERFORMERS="musician primary secoone secotwo secothr tertone terttwo tertthr"
+PERFORMERS="musician primary secondaryone secondarytwo secondarythr tertiaryone tertiarytwo tertiarythr audience"
 
 OUTPUT_DIR=scores/
 
 for ROLE in $PERFORMERS
 do
-    pdflatex -jobname=script-$ROLE -output-directory=$OUTPUT_DIR "\providecommand{\speaker{$ROLE}}\input{raw-score.tex}"
+    pdflatex -jobname=score-$ROLE -output-directory=$OUTPUT_DIR "\providecommand{\speaker{$ROLE}}\input{raw-score.tex}"
 done
+
+pdfbook --short-edge scores/score-audience.pdf --outfile scores/score-audience-book.pdf
 
 # pdflatex -jobname=script-sone '\providecommand{\speaker{sone}}\input{test.tex}'
