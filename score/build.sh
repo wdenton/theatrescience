586cb72cb52984bf7e046f48eb2df589ce41cf90
William Denton <wtd@pobox.com>
2020-02-04 15:05:34 -0500

    Split score in multiple files; more work

--- score/build.sh
+++ score/build.sh
@@ -1,16 +1,20 @@
 
 # https://en.wikibooks.org/wiki/LaTeX/Macros
 
-PERFORMERS="musician primary secondaryone secondarytwo secondarythr tertiaryone tertiarytwo tertiarythr audience"
+PERFORMERS="musician primary secondaryone secondarytwo secondarythr tertiaryone tertiarytwo tertiarythr"
 # PERFORMERS="musician primary secondaryone"
 
-OUTPUT_DIR=scores/
+OUTPUT_DIR=../scores
+
+cd raw
 
 for ROLE in $PERFORMERS
 do
-    pdflatex -jobname=score-$ROLE -output-directory=$OUTPUT_DIR "\providecommand{\speaker{$ROLE}}\input{raw-score.tex}"
+    # pdflatex -jobname=score-$ROLE -output-directory=$OUTPUT_DIR "\providecommand{\speaker{$ROLE}}\input{raw-score.tex}"
+    pdflatex -jobname=score-$ROLE -output-directory=$OUTPUT_DIR "\providecommand{\speaker{$ROLE}}\input{wrapper-performer.tex}"
 done
 
-pdfbook --short-edge scores/score-audience.pdf --outfile scores/score-audience-book.pdf
+pdflatex -jobname=score-audience -output-directory=$OUTPUT_DIR "\providecommand{\speaker{audience}}\input{wrapper-audience.tex}"
+pdfbook --short-edge $OUTPUT_DIR/score-audience.pdf --outfile $OUTPUT_DIR/score-audience-book.pdf
 
 # pdflatex -jobname=script-sone '\providecommand{\speaker{sone}}\input{test.tex}'
